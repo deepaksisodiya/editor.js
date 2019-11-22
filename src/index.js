434b26a4a63eff59157a30615c3478891ca9333a
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2019-11-22 15:29:59 +0530

    added header and list plugin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import List from '@editorjs/List';
 
-const editor = new EditorJS();
+const editor = new EditorJS({
+  headerId: 'editorjs',
+  tools: {
+    header: {
+      class: Header,
+      config: {
+        placeholder: 'Enter a header'
+      }
+    },
+    list: {
+      class: List,
+      inlineToolbar: true,
+    },
+  },
+});
